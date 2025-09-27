16219047fa0480ba986682e965e99c299c3262e1
cevgenerator <cevgenerator@users.noreply.github.com>
2025-09-27 13:11:00 +0200

    Add shader_util.h/.cpp for creating an executable GPU program from the shader source files.

---
+++ src/shader_util.cpp
@@ -0,0 +1,104 @@
+// Copyright (c) 2025 Kars Helderman
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// Implementations of shader utility functions.
+
+#include "shader_util.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+namespace {
+
+constexpr int kShaderInfoLogMaxLength = 512;
+
+}  // namespace
+
+std::optional<std::string> LoadShaderSource(const std::string& path) {
+  std::ifstream file(path);
+
+  if (!file) {
+    return std::nullopt;
+  }
+
+  std::stringstream buffer;
+
+  buffer << file.rdbuf();
+
+  return buffer.str();
+}
+
+// Returns compiled shader ID (wrapped in std::optional) upon succes.
+std::optional<GLuint> CompileShader(GLenum type, const std::string& source) {
+  GLuint shader = glCreateShader(type);  // Unique OpenGL ID.
+  const char* src = source.c_str();      // Convert to raw C-style string.
+
+  glShaderSource(shader, 1, &src, nullptr);  // Attach source.
+  glCompileShader(shader);
+
+  // Check compile status.
+  GLint success = 0;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+  if (success == 0) {
+    char info_log[kShaderInfoLogMaxLength];
+
+    glGetShaderInfoLog(shader, sizeof(info_log), nullptr, info_log);
+
+    std::cerr << "Shader compilation failed:\n" << info_log << '\n';
+
+    glDeleteShader(shader);  // Always clean up.
+
+    return std::nullopt;
+  }
+
+  return shader;
+}
+
+std::optional<GLuint> CreateShaderProgram(const std::string& vertex_path,
+                                          const std::string& fragment_path) {
+  auto vertex_source = LoadShaderSource(vertex_path);
+  if (!vertex_source) {
+    return std::nullopt;
+  }
+
+  auto fragment_source = LoadShaderSource(fragment_path);
+  if (!fragment_source) {
+    return std::nullopt;
+  }
+
+  auto vertex_shader = CompileShader(GL_VERTEX_SHADER, *vertex_source);
+  if (!vertex_shader) {
+    return std::nullopt;
+  }
+
+  auto fragment_shader = CompileShader(GL_FRAGMENT_SHADER, *fragment_source);
+  if (!fragment_shader) {
+    glDeleteShader(*vertex_shader);
+
+    return std::nullopt;
+  }
+
+  // Create shader program object.
+  GLuint program = glCreateProgram();  // Unique program ID.
+  glAttachShader(program, *vertex_shader);
+  glAttachShader(program, *fragment_shader);
+  glLinkProgram(program);  // Create executable GPU program.
+
+  GLint success;
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+  if (success == 0) {
+    glDeleteShader(*vertex_shader);
+    glDeleteShader(*fragment_shader);
+    glDeleteProgram(program);
+
+    return std::nullopt;
+  }
+
+  glDeleteShader(*vertex_shader);
+  glDeleteShader(*fragment_shader);
+
+  return program;
+}
